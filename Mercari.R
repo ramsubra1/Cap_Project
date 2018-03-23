a48346136f8fe4e92c5fb37b3460275ae3de2173
Manasvi Ramakrishnan <ramsubra1@yahoo.com>
2018-03-23 08:28:00 -0700

    Updated the code with Exploratory Analysis

--- Mercari.R
+++ Mercari.R
@@ -1,3 +1,8 @@
+library(ggplot2)
+library(scales)
+library(stringr)
+library(wordcloud)
+library(RColorBrewer)
 library(tidyverse)
 library(data.table)
 library(tidyverse)
@@ -14,9 +19,228 @@ library(doSNOW)
 library(doParallel)
 library(RecordLinkage)
 
+###Open the training file
+df_train <- fread("C://Users/rsubra02/Documents/Mercari/train.tsv")
+train <- df_train
 
-train <- fread("C://Users/rsubra02/Documents/Mercari/train.tsv")
-test <- fread("C://Users/rsubra02/Documents/Mercari/test.tsv")
+### Data Overview
+###Load the 5 Head and 5 tail records of the file
+head(df_train)
+tail(df_train)
+
+##Check the number of records
+dim(df_train)
+
+###Check the size of the file
+print(object.size(df_train), units = 'Mb')
+
+###Check the summary of train data
+summary(df_train)
+
+
+##Varianle Analysis - Price
+###Price Ranges
+range(df_train$price)
+
+ggplot(data = df_train, aes(x = log(price+1))) + 
+  geom_histogram(fill = 'orangered2') +
+  labs(title = 'Histogram of log item price + 1')
+
+##Variable ANalysis - Item Condition
+
+table(df_train$item_condition_id)
+
+##number of items by condition
+
+df_train[, .N, by = item_condition_id] %>%
+  ggplot(aes(x = as.factor(item_condition_id), y = N/1000)) +
+  geom_bar(stat = 'identity', fill = 'cyan2') + 
+  labs(x = 'Item condition', y = 'Number of items (000s)', title = 'Number of items by condition category')
+
+df_train[, .(.N, median_price = median(price)), by = item_condition_id][order(item_condition_id)]
+
+ggplot(data = df_train, aes(x = as.factor(item_condition_id), y = log(price + 1))) + 
+  geom_boxplot(fill = 'cyan2', color = 'darkgrey')
+
+###shipping
+
+table(df_train$shipping)
+
+###Distribution of price by shipping where 1 is by seller and 0 is by purchaser
+df_train %>%
+  ggplot(aes(x = log(price+1), fill = factor(shipping))) + 
+  geom_density(adjust = 6, alpha = 0.8) + 
+  labs(x = 'Log price', y = '', title = 'Distribution of price by shipping')
+
+#plot to find the top 25 median price by brandname
+df_train[, .(median_price = median(price)), by = brand_name] %>%
+  head(25) %>%
+  ggplot(aes(x = reorder(brand_name, median_price), y = median_price)) + 
+  geom_point(color = 'cyan2') + coord_flip()
+
+#find the number of unique categories
+length(unique((df_train$category_name)))
+sort(table(df_train$category_name), decreasing = TRUE)[1:10]
+##Median price by item category
+df_train[, .(median = median(price)), by = category_name][order(median, decreasing = TRUE)][1:30] %>%
+  ggplot(aes(x = reorder(category_name, median), y = median)) + 
+  geom_point(color = 'orangered2') + 
+  coord_flip()
+
+##Finding out the top 10 categories
+
+sort(table(df_train$category_name), decreasing = TRUE)[1:10]
+
+##most of the categories are 3 levels. Let us break them to see how many unique level 1 and 
+#level 2 categories are there and the numbers in each
+# split the item category_name by '/' and get the first two category levels as separate columns
+df_train[, c("level_1_cat", "level_2_cat") := tstrsplit(df_train$category_name, split = "/", keep = c(1,2))]
+
+# peek at the first few rows to make sure this worked correctly. 
+head(df_train[, c("level_1_cat", "level_2_cat")])
+
+##finding the top level categories
+table(df_train$level_1_cat)
+
+##check for the price variation between categories
+df_train %>%
+  ggplot(aes(x = level_1_cat, y = log(price+1))) + 
+  geom_boxplot(fill = 'cyan2', color = 'darkgrey') + 
+  coord_flip() + 
+  labs(x = '', y = 'Log price + 1', title = 'Boxplot of price by top-level category')
+
+# get number of unique level 2 categories
+length(unique(df_train$level_2_cat))
+#Create a box plot for level 2 categories
+df_train %>%
+  ggplot(aes(x = level_2_cat, y = log(price+1))) + 
+  geom_boxplot(fill = 'cyan2', color = 'darkgrey') + 
+  coord_flip() + 
+  labs(x = '', y = 'Log price + 1', title = 'Boxplot of price by second-level category')
+
+p1 <-
+  df_train[, .N, by = c('level_1_cat', 'item_condition_id')] %>%
+  ggplot(aes(x = item_condition_id, y = level_1_cat, fill = N/1000)) +
+  geom_tile() +
+  scale_fill_gradient(low = 'lightblue', high = 'cyan4') +
+  labs(x = 'Condition', y = '', fill = 'Number of items (000s)', title = 'Item count by category and condition') +
+  theme_bw() + 
+  theme(legend.position = 'bottom')
+
+p2 <-
+  df_train[, .(median_price = median(price)), by = c('level_1_cat', 'item_condition_id')] %>%
+  ggplot(aes(x = item_condition_id, y = level_1_cat, fill = median_price)) +
+  geom_tile() +
+  scale_fill_gradient(low = 'lightblue', high = 'cyan4', labels = dollar) +
+  labs(x = 'Condition', y = '', fill = 'Median price', title = 'Item price by category and condition') + 
+  theme_bw() + 
+  theme(legend.position = 'bottom', axis.text.y = element_blank())
+
+grid.arrange(p1, p2, ncol = 2)
+
+##Item Description
+
+#Check if there is a relationship between the length of description and price
+df_train[, desc_length := nchar(item_description)]
+
+# set desc_length to NA where no description exists
+df_train[item_description == 'No description yet', desc_length := NA]
+
+cor(df_train$desc_length, df_train$price, use = 'complete.obs')
+
+df_train[item_description == 'No description yet', item_description := NA]
+
+
+dcorpus <- corpus(df_train$item_description)
+
+dfm1 <- dfm(dcorpus, ngrams = 1, ignoredFeatures = c("rm", stopwords("english")), remove_punct = TRUE, remove_numbers = TRUE, stem = TRUE)
+
+tf <- topfeatures(dfm1, n = 25)
+data.frame(term = names(tf), freq = unname(tf)) %>%
+  ggplot(aes(x = reorder(term, freq), y = freq/1000)) + 
+  geom_bar(stat = 'identity', fill = 'orangered2') + 
+  labs(x = '', y = 'Frequency (000s)', title = '25 most common description words') + 
+  coord_flip() 
+set.seed(100)
+textplot_wordcloud(dfm1, min.freq = 3e4, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+
+dfm2 <- dcorpus %>%
+  corpus_sample(size = floor(ndoc(dcorpus) * 0.15)) %>%
+  dfm(
+    ngrams = 2,
+    ignoredFeatures = c("rm", stopwords("english")),
+    remove_punct = TRUE,
+    remove_numbers = TRUE,
+    concatenator = " "
+  )
+
+# get 25 most common bigrams
+tf <- topfeatures(dfm2, n = 25)
+
+# convert to df and plot
+data.frame(term = names(tf), freq = unname(tf)) %>%
+  ggplot(aes(x = reorder(term, freq), y = freq/1000)) + 
+  geom_bar(stat = 'identity', fill = 'orangered2') + 
+  labs(x = '', y = 'Frequency (000s)', title = '25 most common description bigrams') + 
+  coord_flip() 
+
+set.seed(100)
+textplot_wordcloud(dfm2, min.freq = 2000, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+
+# get 25 most common trigrams
+dfm3 <- dcorpus %>%
+  corpus_sample(size = floor(ndoc(dcorpus) * 0.15)) %>%
+  dfm(
+    ngrams = 3,
+    ignoredFeatures = c("rm", stopwords("english")),
+    remove_punct = TRUE,
+    remove_numbers = TRUE,
+    concatenator = " "
+  )
+
+tf <- topfeatures(dfm3, n = 25)
+
+# convert to df and plot
+data.frame(term = names(tf), freq = unname(tf)) %>%
+  ggplot(aes(x = reorder(term, freq), y = freq/1000)) + 
+  geom_bar(stat = 'identity', fill = 'orangered2') + 
+  labs(x = '', y = 'Frequency (000s)', title = '25 most common description 3-grams') + 
+  coord_flip() 
+
+set.seed(100)
+textplot_wordcloud(dfm3, min.freq = 100, random.order = FALSE,
+                   rot.per = .25, 
+                   colors = RColorBrewer::brewer.pal(8,"Dark2"))
+
+##Add other features as docvars
+docvars(dcorpus, "price") <- df_train$price
+docvars(dcorpus, "brand_name") <- df_train$brand_name
+docvars(dcorpus, "item_condition_id") <- df_train$item_condition_id
+docvars(dcorpus, "level_1_cat") <- df_train$level_1_cat
+docvars(dcorpus, "level_2_cat") <- df_train$level_2_cat
+
+p1 <- summary(dcorpus) %>%
+  ggplot(aes(x = level_1_cat, y = Tokens)) +
+  geom_boxplot(aes(fill = level_1_cat), color = 'grey') +
+  coord_flip() +
+  theme(legend.position = 'bottom') + 
+  labs(x = '', y = 'Number of tokens in description')
+
+p2 <- summary(dcorpus) %>%
+  ggplot(aes(x = Tokens)) +
+  geom_density(aes(fill = level_1_cat), color = 'grey') + 
+  facet_wrap(~level_1_cat) + 
+  theme(legend.position = "none") + 
+  labs(x = 'Number of tokens in description')
+
+grid.arrange(p1, p2, ncol = 2)
+
+
+test <- fread("test.tsv")
 
 #renaming required for binding dataframes
 names(train)[1] <- "id"
@@ -66,9 +290,9 @@ prep_fun = function(x) {
 #Log of price
 #train$LogPrice <- log(train$price+1)
 comb$Class <- ifelse(comb$price<=2.398,"Low",
-                      ifelse(comb$price<=2.89,"Mid",
-                             ifelse(comb$price<=3.401,"High",
-                                    ifelse(comb$price<=5,"Higher","Highest"))))
+                     ifelse(comb$price<=2.89,"Mid",
+                            ifelse(comb$price<=3.401,"High",
+                                   ifelse(comb$price<=5,"Higher","Highest"))))
 
 
 comb$item_description <- prep_fun(comb$item_description)
@@ -107,28 +331,32 @@ Highest <- filter(comb, Class=='Highest')
 HighestWords <- LDAFunc(Highest)
 rm(Low,Mid,High,Higher,Highest)
 
-  train <- comb[1:num_rows_train,]
-  test <- comb[(num_rows_train+1) : nrow(comb),]
-  rm(comb)
-    
-  # test$item_description <- prep_fun(test$item_description)
-    x <- list(LowWords,MidWords,HighWords,HigherWords,HighestWords)
-    classes <- c("Low","Mid","High","Higher","Highest")
-    # test$Class <- vector("character",nrow(test))
-    scores <- vector("numeric",5)
-    cores=detectCores()
-    cl <- makeCluster(cores,outfile="ramout.txt")
-    registerDoSNOW(cl)
-    tic()
-    foreach(i = 1:nrow(test)) %dopar% {
-      for(j in 1:5){
-        scores[j] <- sum(sapply(x[[j]],grepl,test$item_description[i]))
-      }
-      test$Class[i] <- classes[which.max(scores)]
-      print(i)
-    }
-    stopCluster(cl)
-    toc()
+numRowTrain =num_rows_train * 0.1
+
+
+train <- comb[1:round(numRowTrain,digits=0),]
+test <- comb[round(numRowTrain,digits=0) + 1 : round(numRowTrain,digits=0),]
+##test <- comb[(num_rows_train+1) : nrow(comb),]
+rm(comb)
+
+# test$item_description <- prep_fun(test$item_description)
+x <- list(LowWords,MidWords,HighWords,HigherWords,HighestWords)
+classes <- c("Low","Mid","High","Higher","Highest")
+# test$Class <- vector("character",nrow(test))
+scores <- vector("numeric",5)
+cores=detectCores()
+cl <- makeCluster(cores,outfile="ramout.txt")
+registerDoSNOW(cl)
+tic()
+foreach(i = 1:nrow(test)) %dopar% {
+  for(j in 1:5){
+    scores[j] <- sum(sapply(x[[j]],grepl,test$item_description[i]))
+  }
+  test$Class[i] <- classes[which.max(scores)]
+  print(i)
+}
+stopCluster(cl)
+toc()
 
 ########################################################################
 #Modeling
@@ -171,7 +399,7 @@ for (depth in seq(1,20,1))  {
     
     # predict
     predictions <- abs(predict(bst, as.matrix(testSet[,predictors]), outputmargin=TRUE))
-    err <- Metrics::rmsle((as.numeric(testSet[,outcomeName])+1), (as.numeric(predictions)+1))
+    err <- Metrics::rmse((as.numeric(testSet[,outcomeName])+1), (as.numeric(predictions)+1))
     
     if (err < smallestError) {
       smallestError = err
@@ -205,7 +433,7 @@ for (depth in seq(1,10,1)) {
       gc()
       predictions <- predict(bst, as.matrix(dataTest[,predictors]), outputmargin=TRUE)
       
-      err <- rmsle(as.numeric(dataTest[,outcomeName]), as.numeric(predictions))
+      err <- rmse(as.numeric(dataTest[,outcomeName]), as.numeric(predictions))
       totalError <- c(totalError, err)
     }
     if (mean(totalError) < smallestError) {
@@ -213,40 +441,41 @@ for (depth in seq(1,10,1)) {
       print(paste(depth,rounds,smallestError))
     }  
   }
-} 
+}   
 
 #Testing the models
 trainSet <- myTrainDmy[ 1:trainPortion,]
 
 # assign everything else to test
-testSet <- myTrainDmy[(trainPortion+1):nrow(myTrainDmy),]
+testSet <- myTrainDmy[(trainPortion+1):nrow(myTrainDmy)/2,]
 
+bst_final <- xgboost(data = as.matrix(trainSet[,predictors]),
+                     label = trainSet[,outcomeName],
+                     max.depth=7, nround=20, objective = "reg:linear", verbose=0)
+
+importance_matrix <- xgb.importance(predictors, model = bst_final)
+
+xgb.plot.importance(importance_matrix[1:15], rel_to_first = TRUE, xlab = "Relative importance")
+
+
+(gg <- xgb.ggplot.importance(importance_matrix[1:15], measure = "Frequency", rel_to_first = TRUE))
+gg + ggplot2::ylab("Frequency")
+topPredictors <- cat(toString(importance_matrix[1:15,1]))
+#pred <- predict(bst, as.matrix(testSet[,topPredictors]), outputmargin=TRUE)
+pred <- predict(bst_final, as.matrix(testSet[,c("shippingT", "item_condition_id", "len_words", "cat_2Shoes", "cat_2Women.s.Handbags", "avg_len_words", "num_words", "cat_1Women", "cat_2Tops...Blouses", "cat_1Electronics", "cat_1Men", "cat_2Jewelry", "cat_2Athletic.Apparel", "cat_1Handmade", "cat_2Computers...Tablets")]), outputmargin=TRUE)
+rmse(as.numeric(testSet[,outcomeName]), as.numeric(pred))
+submission <- as.data.frame(cbind(train$id[(trainPortion+1):nrow(myTrainDmy)/2],exp(pred), pred))
+colnames(submission) <- c("test_id","price", "log price")
+####=========================================================
 bst <- xgboost(data = as.matrix(trainSet[,predictors]),
                label = trainSet[,outcomeName],
                max.depth=8, nround=9, objective = "reg:linear", verbose=0)
 pred <- predict(bst, as.matrix(testSet[,predictors]), outputmargin=TRUE)
-rmsle(as.numeric(testSet[,outcomeName]), as.numeric(pred))
-submission <- as.data.frame(cbind(train$id[(trainPortion+1):nrow(myTrainDmy)],pred))
-colnames(submission) <- c("test_id","price")
-write.csv(submission,"sub1_6_9.csv")
-#Let's try this on the test dataset
-#Select the variables
-myTest <- test %>% select(item_condition_id,shipping,ItemDescLen,CategoryTags,level1cat,Class)
+rmse(as.numeric(testSet[,outcomeName]), as.numeric(pred))
 
-#Binarize all factors
-dmy <- dummyVars("~.",data=myTest)
-myTestDmy <- data.frame(predict(dmy, newdata = myTest))
-pred <- predict(bst,as.matrix(myTestDmy[,predictors]),outputmargin = TRUE)
+submission <- as.data.frame(cbind(train$id[(trainPortion+1):nrow(myTrainDmy)/2],exp(pred), pred))
+colnames(submission) <- c("test_id","price", "log price")
+write.csv(submission,"sub1_6_9_1.csv")
 
-submission <- as.data.frame(cbind(test$test_id,pred))
-colnames(submission) <- c("test_id","price")
-write.csv(submission,"sub1_6_9.csv")
-
-
-bst <- xgboost(data = as.matrix(trainSet[,predictors]),
-               label = trainSet[,outcomeName],
-               max.depth=3, nround=20, objective = "reg:linear", verbose=0)
-pred <- predict(bst, as.matrix(testSet[,predictors]), outputmargin=TRUE)
-rmsle(as.numeric(testSet[,outcomeName]), as.numeric(pred))
 
 
